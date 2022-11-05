9062835c93b81dcf78377e016229c6af2786bbab
joshua constine <joshua.constine97@gmail.com>
2022-11-05 10:25:51 -0700

    added logo and updated navbar styles

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,30 @@
 import Link from "next/link";
 import classes from "./Navbar.module.css";
+import Image from "next/image";
+
 function Navbar() {
   return (
     <nav className={classes.nav}>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/restaurants">Restaurants</Link>
-        </li>
-      </ul>
+      <div
+        style={{
+          position: "relative",
+          display: "inline-block",
+          width: "300px",
+          height: "80px",
+        }}
+      >
+        <Image src="/static/logo.png" alt="lgoo" layout="fill" />
+      </div>
+      <div>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/restaurants">Restaurants</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
