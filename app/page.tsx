f233bb375be8ab75cbfd05520631e497ed847a10
joshua constine <joshua.constine97@gmail.com>
2022-11-05 14:04:01 -0700

    modified home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,23 @@
 "use client";
-import Image from "next/image";
-import styles from "./page.module.css";
-import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
-import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
-import Account from "../components/Account";
+import Link from "next/link";
 
 export default function Home() {
-  const session = useSession();
-  const supabase = useSupabaseClient();
   return (
     <div className="container" style={{ padding: "50px 0 100px 0" }}>
-      {!session ? (
-        <div className="row">
-          <div className="col-6">
-            <h1 className="header">Supabase Auth + Storage</h1>
-            <p className="">
-              Experience our Auth and Storage through a simple profile
-              management example. Create a user profile and upload an avatar
-              image. Fast, simple, secure.
-            </p>
-          </div>
-          <div className="col-6 auth-widget">
-            <Auth
-              supabaseClient={supabase}
-              appearance={{ theme: ThemeSupa }}
-              theme="dark"
-            />
-          </div>
-        </div>
-      ) : (
-        <>
-          <h3>Account</h3>
-          <Account session={session} />
-        </>
-      )}
+      <h1>Welcome to Foodie </h1>
+      <Link
+        href={`/restaurants`}
+        style={{
+          fontSize: "22px",
+          paddingTop: "20px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        Restaurants
+      </Link>
     </div>
   );
 }
