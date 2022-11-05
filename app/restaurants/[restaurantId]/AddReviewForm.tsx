084c49402e27b5c2a7c36e2d3a0007da6f83e2ab
joshua constine <joshua.constine97@gmail.com>
2022-11-05 10:57:53 -0700

    refactored review to controlled component

--- app/restaurants/[restaurantId]/AddReviewForm.tsx
+++ app/restaurants/[restaurantId]/AddReviewForm.tsx
@@ -1,10 +1,16 @@
 "use client";
 
+import { useState } from "react";
+
 const AddReviewForm = () => {
+  const [review, setReview] = useState("");
+  const handleChange = (e: any) => {
+    setReview(e.target.value);
+  };
   return (
-    <form onSubmit={() => window.alert("hello")}>
+    <form onSubmit={() => window.alert(review)}>
       <label>leave review</label>
-      <textarea rows={4} />
+      <textarea rows={4} name="review" value={review} onChange={handleChange} />
       <button type="submit">add</button>
     </form>
   );
