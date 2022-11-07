d89c1bb06501a8652fcef9ffc4148c5067c6ebe5
joshua constine <joshua.constine97@gmail.com>
2022-11-07 14:56:15 -0800

    added next auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,25 @@
+"use client";
 import Navbar from "../components/Navbar";
 import "./globals.css";
+import { SessionProvider } from "next-auth/react";
 
-export default function RootLayout({ children }: any) {
+export default function RootLayout({ children, ...props }: any) {
   return (
-    <html lang="en">
-      <head>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
-        />
-      </head>
-      <body>
-        <header>
-          <Navbar />
-        </header>
-        {children}
-      </body>
-    </html>
+    <SessionProvider session={props.session}>
+      <html lang="en">
+        <head>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+          />
+        </head>
+        <body>
+          <header>
+            <Navbar />
+          </header>
+          {children}
+        </body>
+      </html>
+    </SessionProvider>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,64 @@
+"use client";
 import Link from "next/link";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
+  const { data: session } = useSession();
+  console.log(session);
+  if (session) {
+    return (
+      <>
+        Signed in as {session?.user?.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
   return (
-    <div
-      className="container"
-      style={{
-        padding: "50px 0 100px 0",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <h1>Welcome to Foodie </h1>
-      <Link
-        href={`/restaurants`}
-        style={{
-          fontSize: "22px",
-          paddingTop: "20px",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <button
-          style={{
-            background: "#d6ccc2",
-            borderRadius: "999px",
-            boxShadow: "#403D39 0 10px 20px -10px",
-            boxSizing: "border-box",
-            color: "black",
-            cursor: "pointer",
-            fontSize: "16px",
-            outline: "none",
-            border: "none",
-            padding: "8px 18px",
-          }}
-        >
-          Restaurants
-        </button>
-      </Link>
-    </div>
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
   );
+  // return (
+  //   <div
+  //     className="container"
+  //     style={{
+  //       padding: "50px 0 100px 0",
+  //       display: "flex",
+  //       flexDirection: "column",
+  //       justifyContent: "center",
+  //       alignItems: "center",
+  //     }}
+  //   >
+  //     <h1>Welcome to Foodie </h1>
+  //     <Link
+  //       href={`/restaurants`}
+  //       style={{
+  //         fontSize: "22px",
+  //         paddingTop: "20px",
+  //         display: "flex",
+  //         flexDirection: "column",
+  //         justifyContent: "center",
+  //         alignItems: "center",
+  //       }}
+  //     >
+  //       <button
+  //         style={{
+  //           background: "#d6ccc2",
+  //           borderRadius: "999px",
+  //           boxShadow: "#403D39 0 10px 20px -10px",
+  //           boxSizing: "border-box",
+  //           color: "black",
+  //           cursor: "pointer",
+  //           fontSize: "16px",
+  //           outline: "none",
+  //           border: "none",
+  //           padding: "8px 18px",
+  //         }}
+  //       >
+  //         Restaurants
+  //       </button>
+  //     </Link>
+  //   </div>
+  // );
 }
