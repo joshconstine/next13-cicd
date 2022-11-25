0539bfe83c5e85c6d9b932ee71df8cb54c9b5e2f
joshua constine <joshua.constine97@gmail.com>
2022-11-25 15:38:20 -0800

    added image component

--- app/restaurants/layout.tsx
+++ app/restaurants/layout.tsx
@@ -3,11 +3,39 @@ import Link from "next/link";
 import Card from "../../components/card";
 import { PrismaClient } from '@prisma/client'
 import React from "react";
+import S3 from 'aws-sdk/clients/s3'
+import Image from "next/image";
+
+
 const prisma = new PrismaClient()
 async function RestaurantsLayout({ children }: any) {
 
   const data = await prisma.restaurant.findMany()
   await prisma.$disconnect()
+  const s3 = new S3({
+    region: 'us-west-1',
+    accessKeyId: process.env.ACCESS_KEY,
+    secretAccessKey: process.env.SECRET_KEY,
+
+  })
+  const getPhotoUrl = (): string => {
+
+    const restaurantPhotosKey = encodeURIComponent('restaurants/1') + '/'
+    s3.listObjects({ Bucket: 'foodly-storage-bucket', Prefix: '' }, (err, data) => {
+      if (err) {
+        console.log('error', err)
+        return ''
+      } else {
+        const image = data.Contents?.find(elem => (elem?.Size && elem?.Size > 0))
+        console.log('----------------')
+        console.log(process.env.BUCKET_URL + '/' + encodeURIComponent(image?.Key ? image.Key : ''))
+        return process.env.BUCKET_URL + '/' + encodeURIComponent(image?.Key ? image.Key : '')
+
+      }
+
+    })
+    return ''
+  }
   return (
     <div className={classes.layout}>
       <aside className={classes.sidebar}>
@@ -36,6 +64,7 @@ async function RestaurantsLayout({ children }: any) {
                     alignItems: "center",
                   }}
                 >
+                  <Image width={200} height={200} src={getPhotoUrl()} alt='img' />
                   <Card
                     text={restaurant.name}
                     size="large"
